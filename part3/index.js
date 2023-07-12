e7feb44439d70ef4df9f0ff6f32b8e9c68bf9933
simon9141 <simonotieno9141@gmail.com>
2023-07-12 15:21:40 +0300

    part2 build

--- part3/index.js
+++ part3/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const morgan =require('morgan')
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan('tiny'))
 const persons= [
@@ -80,7 +82,7 @@ function generateId() {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-const PORT = 3000
+const PORT = process.env.PORT || 3001
 app.listen(PORT,()=>{
     console.log(`server running at port ${PORT}`)
 })
